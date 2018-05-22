889d87aff0c276af59ca54686a776704aa65b97b
HankyWang <HankyWang@users.noreply.github.com>
2018-05-22 22:05:17 +0800

    add basic tcpserver

--- airsys/app.py
+++ airsys/app.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpRequest
 import subprocess
 import os,sys
-import socket as soc
+import socket
 
 def stats():
     ctx = {}
@@ -16,3 +16,22 @@ def get_ip():
     	"grep -v inet6|"
     	"awk '{print $2}'"
     	"|tr -d 'addr:'", shell=True).strip()
+
+HOST = get_ip()
+PORT = 8880
+BUF_SIZE = 1024
+
+def handle_request(conn):
+    msg = conn.recv(BUF_SIZE)
+    conn.send(msg)         #to client
+
+def server(client):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((HOST,PORT))
+    sock.listen(5)
+
+    while True:
+        connection, address = sock.accept()
+        handle_request(connection)
+
+
--- airsys/data.py
+++ airsys/data.py
@@ -2,8 +2,36 @@ import threading
 
 
 ROOM_NUM = 4
+max_temp = 28
+min_temp = 16
+def_temp = 30
 
-class Room{
+COOL = 0
+HEAT = 1
+mode = COOL
 
 
-}
+class Room:
+
+    # Status
+    IDLE = 0
+    RUNNING = 1
+    SUSPENDED = 2
+    DISCONNECTED = 3
+
+    # Wind Speed
+    LOW = 0
+    MEDIUM = 1
+    HIGH = 2
+
+    def __init__(
+            self,
+            id,
+            is_checkin,
+            is_connected,
+            status,
+
+    ):
+        pass
+
+
